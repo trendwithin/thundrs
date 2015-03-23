a21780c9f1c21e28b8b6825b17b3a4e7b986fa02
Tim Heuett <tim.heuett@gmail.com>
2015-03-23 11:32:18 -0700

    add happy test for viewing other user's memories

--- test/features/memory/show_test.rb
+++ test/features/memory/show_test.rb
@@ -7,14 +7,26 @@ feature "As a user, I would like to see the details of memories" do
   end
 
   scenario "users can click on memory images to see the detail page" do
-    # given a specific memory
+    # given one of their own memories
     @memory = memories :user1_memory
 
-    # when it is clicked on in the index
+    # when the user clicks on it in the index
     visit memories_path
     page.find("#memory .memory-image[src=#{memory_data.image_src}]").click
 
     # then the user will be directed to the memory's detail page
     current_path.must_equal memory_path(@memory)
+    page_must_include_memory(@memory)
+  end
+
+  scenario "users can view other user's memory details" do
+    # given one of another user's memories
+    @memory = memories :user2_memory
+
+    # when the user visits the detail page
+    visit memory_path(@memory)
+
+    # then the user can see that memory
+    page_must_include_memory(@memory)
   end
 end
