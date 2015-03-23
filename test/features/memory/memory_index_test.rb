e667cd32dea076db4965f2ac7ffdcb8b9701ad9a
Tim Heuett <tim.heuett@gmail.com>
2015-03-23 11:16:43 -0700

    dry up test and add GWT comments

--- test/features/memory/memory_index_test.rb
+++ test/features/memory/memory_index_test.rb
@@ -1,20 +1,31 @@
 require "test_helper"
 
 feature "As a user, I would like to have a personal landing page where I can view my memories" do
-  scenario "memory index shows personal memories" do
-    visit memories_path
+  before do
     user = users :user1
+    sign_in user
+  end
+
+  scenario "memory index shows personal memories" do
+    # given a user's own memory
     memory = memories :user1_memory
 
+    # when the user views their memory list
+    visit memories_path
+
+    # then the memory should be displayed
     page.text.must_include memory.name
     page.find("#memory#{memory.id} .memory-image")['src'].must_have_content memory.image_src
   end
 
   scenario "memory index doesn't show other's memories" do
-    visit memories_path
-    user = users :user1
+    # given some other user's memory
     memory = memories :user2_memory
 
+    # when the user views their memory list
+    visit memories_path
+
+    # then the memory should not be displayed
     page.text.wont_include memory.name
     assert_raise Capybara::ElementNotFound do
       page.find("#memory#{memory.id}")
