ed3ed3f340dd4d13a230e25315e916f94680904a
Tim Heuett <tim.heuett@gmail.com>
2015-03-24 10:32:09 -0700

    add tests for the 'admin' user role

---
+++ test/features/authz/admin_privileges_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+feature "As an administrator, I want to delete content from the site, so that I can remove inappropriate content" do
+  scenario "admins can delete memories" do
+  end
+
+  scenario "admins can delete comments" do
+  end
+
+  scenario "non-admins cannot delete memories" do
+  end
+
+  scenario "non-admins cannot delete comments" do
+  end
+end
+
+feature "As an administrator, I want to ban abusive users from the site" do
+  scenario "admins can ban other users" do
+  end
+
+  scenario "normal users cannot ban other users" do
+  end
+
+  scenario "banned users cannot login" do
+  end
+end
