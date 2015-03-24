baeb731c87fb41f1b8ff176f725f540f541e4fc8
Tim Heuett <tim.heuett@gmail.com>
2015-03-24 14:15:18 -0700

    next comment resource under memories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :memories
+  resources :memories do
+    resources :comments
+  end
   devise_for :users
+
   root 'thundrs#index'
 end
