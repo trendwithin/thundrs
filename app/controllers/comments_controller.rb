ec0bee9c90be322bfb7e9475ed493f621d36860d
Michael Becco <e4e6d4d5@gmail.com>
2015-03-24 20:30:24 -0700

    Replace commented out code in Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -49,7 +49,6 @@ class CommentsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def comment_params
-    params.require(:comment).permit(:body, :creator_id, :memory_id, :approved)
-    # params.require(:comment).permit(*policy(@comment || Comment).permitted_attributes)
+    params.require(:comment).permit(*policy(@comment || Comment).permitted_attributes)
   end
 end
