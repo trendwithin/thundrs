6d93a10936551c4f26056a39599babe24d1ba917
Tim Heuett <tim.heuett@gmail.com>
2015-03-25 11:14:55 -0700

    oops! that doesn't go there

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -2,6 +2,8 @@ class Memory < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   has_many :comments
 
+  has_attached_file :image
+
   validates :name, presence: true
   validates :keywords, presence: true
   validates :description, presence: true
