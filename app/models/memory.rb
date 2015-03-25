778ee11af01a29e50d9f9c6877d2377cf1d51a2a
Tim Heuett <tim.heuett@gmail.com>
2015-03-25 11:28:22 -0700

    add default url to memory model and fixtures

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -2,7 +2,7 @@ class Memory < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   has_many :comments
 
-  has_attached_file :image
+  has_attached_file :image, default_url: "/img-300-placeholder.gif"
 
   validates :name, presence: true
   validates :keywords, presence: true
