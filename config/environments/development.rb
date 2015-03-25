55fc551882549101bb0aa503b7c66bac7df27bc8
Tim Heuett <tim.heuett@gmail.com>
2015-03-24 19:47:08 -0700

    add s3 creds to paperclip config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Paperclip credentials for S3
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket:               Rails.application.secrets.s3_bucket_name,
+      access_key_id:        Rails.application.secrets.aws_access_key_id,
+      secret_access_key:    Rails.application.secrets.aws_secret_access_key
+    }
+  }
 end
