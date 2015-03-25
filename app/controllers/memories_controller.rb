bea25d0fe317efc051c0b6405dd9797c70493b6c
Michael Becco <e4e6d4d5@gmail.com>
2015-03-24 20:38:00 -0700

    Add Devise Auth to Controllers

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,4 +1,5 @@
 class MemoriesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_memory, only: [:show, :edit, :update, :destroy]
   after_action :verify_authorized, only: [:update, :edit]
   # GET /memories
