5e8084bb6caccde17014fdf1ba6e139808b84ce3
Tim Heuett <tim.heuett@gmail.com>
2015-03-25 16:34:41 -0700

    and another

--- db/migrate/20150323230030_create_comments.rb
+++ db/migrate/20150323230030_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :body
-      t.references :author, index: true, foreign_key: true
+      t.references :author, index: true
       t.references :memory, index: true, foreign_key: true
       t.boolean :approved, default: false
 
