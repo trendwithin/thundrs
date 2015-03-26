40b6e32f8df3e09695ebafd31226a632f54294e7
Tim Heuett <tim.heuett@gmail.com>
2015-03-26 10:30:14 -0700

    add keywords table and keyword_memory join

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,6 +1,7 @@
 class Memory < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   has_many :comments
+  has_and_belongs_to_many :keywords
 
   has_attached_file :image, default_url: "/img-300-placeholder.gif"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
