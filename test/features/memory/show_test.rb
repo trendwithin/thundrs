76684f88b73362759b606b7533038a7bee5e4fe2
Tim Heuett <tim.heuett@gmail.com>
2015-03-27 09:51:08 -0700

    capybara can't click on images inside link tags

--- test/features/memory/show_test.rb
+++ test/features/memory/show_test.rb
@@ -6,20 +6,6 @@ feature "As a user, I would like to see the details of memories" do
     sign_in @user
   end
 
-  scenario "users can click on memory images to see the detail page" do
-    skip
-    # given one of their own memories
-    @memory = memories :user1_memory
-
-    # when the user clicks on it in the index
-    visit memories_path
-    page.find("#memory-#{@memory.id}.memory .memory-image").click
-
-    # then the user will be directed to the memory's detail page
-    current_path.must_equal memory_path(@memory)
-    page_must_include_memory(@memory)
-  end
-
   scenario "users can view other user's memory details" do
     # given one of another user's memories
     @memory = memories :user2_memory
