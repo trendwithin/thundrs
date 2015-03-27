5fd01f8d8e2ec8d48ceba267341895861aecdad5
Tim Heuett <tim.heuett@gmail.com>
2015-03-26 17:23:50 -0700

    remove test
    user stories no longer support this requirement

--- test/features/memory/memory_index_test.rb
+++ test/features/memory/memory_index_test.rb
@@ -17,18 +17,4 @@ feature "As a user, I would like to have a personal landing page where I can vie
     page.text.must_include memory.name
     # page.find("#memory#{memory.id} .memory-image")['src'].must_have_content memory.image_src
   end
-
-  scenario "memory index doesn't show other's memories" do
-    # given some other user's memory
-    memory = memories :user2_memory
-
-    # when the user views their memory list
-    visit memories_path
-
-    # then the memory should not be displayed
-    page.text.wont_include memory.name
-    assert_raise Capybara::ElementNotFound do
-      page.find("#memory#{memory.id}")
-    end
-  end
 end
