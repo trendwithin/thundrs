7a76e5c802b6d20c3673c5e44bb93d2b6ab1e115
Tim Heuett <tim.heuett@gmail.com>
2015-03-26 22:45:48 -0700

    simplify using active record methods

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -6,7 +6,7 @@ class MemoriesController < ApplicationController
   def index
     memories = policy_scope(Memory)
     @recent_memories = memories.where.not(creator: current_user).order('created_at DESC').last(6)
-    @personal_memories = memories.where(creator: current_user).order('created_at DESC')
+    @personal_memories = current_user.memories.order('created_at DESC')
 
     # TODO: refactor this into a relationship using group?
     @replies = {}
