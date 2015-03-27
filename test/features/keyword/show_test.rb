47e82d20aed252323e47466b21ba9e05a44e994f
Tim Heuett <tim.heuett@gmail.com>
2015-03-26 19:19:14 -0700

    passing test for memories on keyword page

--- test/features/keyword/show_test.rb
+++ test/features/keyword/show_test.rb
@@ -3,10 +3,11 @@ require "test_helper"
 feature "As a user, I want to click on keywords to see memories associated with them" do
   scenario "keywords have a show template with memories" do
     keyword = keywords :k1
+    sign_in users(:user_1)
 
     visit keyword_path(keyword)
 
-    page_must_include_memory memories(:user1_memory)
-    page_must_include_memory memories(:user2_memory)
+    page.find("#memory-#{memories(:user1_memory).id}").wont_be_nil
+    page.find("#memory-#{memories(:user2_memory).id}").wont_be_nil
   end
 end
