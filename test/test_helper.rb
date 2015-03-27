feaf142079c672c037d44137906acd5931481000
Tim Heuett <tim.heuett@gmail.com>
2015-03-27 08:51:11 -0700

    can't check for keywords on those fixtures because they don't match the string

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -45,7 +45,6 @@ class ActiveSupport::TestCase
 
   def page_must_include_memory(memory_data)
     page.text.must_include memory_data.name
-    page.text.must_include memory_data.keywords
     # page.text.must_include memory_data.description
     # TODO: this next test is a little weird. If it's nil, it will actually raise an error, but not sure
     #       how best to check for that
@@ -54,7 +53,6 @@ class ActiveSupport::TestCase
 
   def page_wont_include_memory(memory_data)
     page.text.wont_include memory_data.name
-    page.text.wont_include memory_data.keywords
     page.text.wont_include memory_data.description
     assert_raise Capybara::ElementNotFound do
       page.find("#memory-#{memory_data.id}.memory .memory-image")
